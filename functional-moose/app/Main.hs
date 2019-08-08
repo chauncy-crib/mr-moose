658f85eb6235e0f688d6acb87f213f05cc1feb0c
David Abrahams <davidabrahams@stripe.com>
2019-08-08 15:29:36 -0700

    anagram algorithm seems to work

--- functional-moose/app/Main.hs
+++ functional-moose/app/Main.hs
@@ -1,6 +1,31 @@
 module Main where
 
-import           Lib
+import           Anagram
+import qualified Data.MultiSet                 as MultiSet
+
+-- represent our dictionary as a function from word length to a list of words of that length
+dict :: Int -> [String]
+dict 1 = ["a"]
+dict 3 = ["cat", "dog"]
+dict 4 = ["poop"]
+dict 5 = ["butts"]
+dict _ = []
+
+poop = "poop"
+poopdog = "pogdoop"
+catdog = "dcatgo"
+poopbutts = "potoubpts"
+buttspoopcatdog = "godtacpoopsttbu"
+nosolution1 = "godtacpoopsttb"
+nosolution2 = "godtacpoopsttbuu"
+
+-- Some test cases, all working
 
 main :: IO ()
-main = someFunc
+-- main = putStrLn $ show (solve (MultiSet.fromList poopdog) dict [4, 3])
+main =
+  putStrLn $ show (solve (MultiSet.fromList buttspoopcatdog) dict [5, 4, 3, 3])
+-- main =
+  -- putStrLn $ show (solve (MultiSet.fromList nosolution1) dict [5, 4, 3, 3])
+-- main = putStrLn $ show (solve (MultiSet.fromList "a") dict [1])
+-- main = putStrLn $ show (solve (MultiSet.fromList "a") dict [1])
--- functional-moose/src/Anagram.hs
+++ functional-moose/src/Anagram.hs
@@ -1,32 +1,51 @@
 
-module Anagram where
+module Anagram
+  ( solve
+  )
+where
 
 import           Data.MultiSet                  ( MultiSet )
 import qualified Data.MultiSet                 as MultiSet
 
 -- Given the characters remaining, and a list of words to try, return the first
--- word which can be spelled, along with the remaining untried words.
-findWord :: (MultiSet Char) -> [String] -> (Maybe (String, [String]))
+-- word which can be spelled, the remaining untried words, and the remaining characters
+findWord
+  :: (MultiSet Char) -> [String] -> (Maybe (String, [String], (MultiSet Char)))
 findWord remainingChars [] = Nothing
 findWord remainingChars (word : rest) =
-  if ((MultiSet.fromList word) `MultiSet.isSubsetOf` remainingChars)
-    then (Just (word, rest))
+  if (wordSet `MultiSet.isSubsetOf` remainingChars)
+    then (Just (word, rest, MultiSet.difference remainingChars wordSet))
     else findWord remainingChars rest
+  where wordSet = MultiSet.fromList word
+
+
+solveCurrentWord
+  :: [String] -- the current list of candidate words. Should all be the same length
+  -> (MultiSet Char) -- the bag of characters remaining
+  -> [Int] -- the word lengths we need to form, after this word.
+  -> (Int -> [String]) -- the dictionary
+  -> (Maybe [String]) -- the solution to the whole problem
+solveCurrentWord currentWords remainingChars remainingLengths dict =
+  let firstWord = findWord remainingChars currentWords
+  in  case firstWord of
+        Just (word, rest, rem) ->
+          let maybeSoln = solve rem dict remainingLengths
+          in  case maybeSoln of
+                Just words -> Just (word : words) -- if we have found a solution, return it
+                Nothing ->
+                  solveCurrentWord rest remainingChars remainingLengths dict -- otherwise, try a new word
+        _ -> Nothing
+
 
 -- Given a set of characters remaining, a dictionary (represented as a function from
 -- word length to List of Strings), a list of word lengths remaining, return the
 -- first solution to the anagram
 solve :: (MultiSet Char) -> (Int -> [String]) -> [Int] -> (Maybe [String])
-solve remainingChars dict remainingWordLengths = case remainingWordLengths of
-  wordLength : rest ->
-    let words    = dict wordLength
-        thisWord = findWord remainingChars words
-    in  case thisWord of
-          Just (word, rest) -> Nothing
-          _                 -> Nothing
+solve remainingChars _ [] = if null remainingChars then Just [] else Nothing -- if we have a solution, we expect to have no characters in the bag
+solve remainingChars dict (wordLength : rest) =
+  let words = dict wordLength
+  in  solveCurrentWord words remainingChars rest dict
 
-
---   _ -> Just []
 
 
 
